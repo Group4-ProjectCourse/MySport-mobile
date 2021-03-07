d8c01124174f9a6d681e833c50da92ad713183d4
kristijan00 <57765162+kristijan00@users.noreply.github.com>
2021-03-07 10:09:11 +0100

    SettingsFragment fix

--- app/src/main/java/com/mysport/mysport_mobile/settings/SettingsFragment.java
+++ app/src/main/java/com/mysport/mysport_mobile/settings/SettingsFragment.java
@@ -11,20 +11,22 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import com.mysport.mysport_mobile.R;
+import org.jetbrains.annotations.NotNull;
 
 public class SettingsFragment extends Fragment {
 
-    private final String[] languages = { "English", "Swedish", "Norwegian" };
+    private final String[] languages = {"English", "Swedish", "Norwegian"};
     private Spinner spinnerLanguage;
     private Spinner spinnerColour;
+    private ArrayAdapter<?> adapter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_day_table, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
         spinnerLanguage = view.findViewById(R.id.spinnerLanguage);
         spinnerColour = view.findViewById(R.id.spinnerColour);
-        ArrayAdapter adapter = new ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_dropdown_item,  languages);
+        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, languages);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerLanguage.setAdapter(adapter);
         spinnerLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -41,5 +43,28 @@ public class SettingsFragment extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        spinnerLanguage = view.findViewById(R.id.spinnerLanguage);
+//        spinnerColour = view.findViewById(R.id.spinnerColour);
+//        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, languages);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinnerLanguage.setAdapter(adapter);
+//        spinnerLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                String selectedItem = adapterView.getItemAtPosition(i).toString();
+//                Toast.makeText(adapterView.getContext(), "Selected: " + selectedItem, Toast.LENGTH_LONG).show();
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//            }
+//        });
     }
 }

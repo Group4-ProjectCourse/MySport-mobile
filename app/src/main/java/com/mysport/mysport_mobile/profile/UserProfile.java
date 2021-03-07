66b4b522c4e4f74ab00b79d79b70009b0b338055
Peter-Daniel-hkr <petrus.daniel@protonmail.com>
2021-03-07 03:07:19 +0100

    Firebase Authentication
    * Firebase Authentication:
    - Authenticate using Email and Password
    - Authenticate using Facebook Login
    * Realtime Database
    * Cloud Storage

--- app/src/main/java/com/mysport/mysport_mobile/profile/UserProfile.java
+++ app/src/main/java/com/mysport/mysport_mobile/profile/UserProfile.java
@@ -6,10 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+<<<<<<< Updated upstream
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+=======
+>>>>>>> Stashed changes
 import com.mysport.mysport_mobile.R;
 
 public class UserProfile extends Fragment {
@@ -17,14 +20,17 @@ public class UserProfile extends Fragment {
 
 
     @Override
+<<<<<<< Updated upstream
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         return view;
+=======
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
 
+>>>>>>> Stashed changes
     }
 }

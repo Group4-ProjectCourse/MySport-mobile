ade870e66094fd0f9581b90feb56f7c56099a2d2
Peter-Daniel-hkr <petrus.daniel@protonmail.com>
2021-03-07 04:49:25 +0100

    Resolved merge conflict

--- app/src/main/java/com/mysport/mysport_mobile/profile/UserProfile.java
+++ app/src/main/java/com/mysport/mysport_mobile/profile/UserProfile.java
@@ -2,17 +2,13 @@ package com.mysport.mysport_mobile.profile;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
-<<<<<<< Updated upstream
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-=======
->>>>>>> Stashed changes
+
 import com.mysport.mysport_mobile.R;
 
 public class UserProfile extends Fragment {
@@ -20,17 +16,16 @@ public class UserProfile extends Fragment {
 
 
     @Override
-<<<<<<< Updated upstream
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-
         return view;
-=======
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile);
 
-
->>>>>>> Stashed changes
-    }
+  //  protected void onCreate(Bundle savedInstanceState) {
+  //      super.onCreate(savedInstanceState);
+  //      setContentView(R.layout.activity_user_profile);
+//
+//
+//
+   }
 }

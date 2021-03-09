55d5e65d4d2dd41af991835a454c5e1a091900b7
Gormsson <gormsson2@gmail.com>
2021-03-09 14:06:31 +0100

    Color changes (WIP)
    Added some themes to be able to dynamically change colors at runtime.
    TODO: make selection of color affect MainActivity.

--- app/src/main/java/com/mysport/mysport_mobile/fragments/settings/SettingsFragment.java
+++ app/src/main/java/com/mysport/mysport_mobile/fragments/settings/SettingsFragment.java
@@ -10,30 +10,38 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.mysport.mysport_mobile.MainActivity;
 import com.mysport.mysport_mobile.R;
 import org.jetbrains.annotations.NotNull;
 
 public class SettingsFragment extends Fragment {
 
-    private final String[] languages = { "English", "Swedish", "Norwegian" };
+    private final String[] languages = { "English", "Svenska", "Norsk" };
+    private final String[] colours = {"Purple", "Blue", "Black", "Dark Green"};
     private Spinner spinnerLanguage;
     private Spinner spinnerColour;
-    private ArrayAdapter<?> adapter;
+    private ArrayAdapter<?> adapterLanguage, adapterColour;
+    private Button buttonSave;
+    String colourSelected, languageSelected;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
+        buttonSave = view.findViewById(R.id.buttonSave);
         spinnerLanguage = view.findViewById(R.id.spinnerLanguage);
         spinnerColour = view.findViewById(R.id.spinnerColour);
-        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, languages);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerLanguage.setAdapter(adapter);
+        adapterLanguage = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, languages);
+        adapterLanguage.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLanguage.setAdapter(adapterLanguage);
         spinnerLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 String selectedItem = adapterView.getItemAtPosition(i).toString();
-                Toast.makeText(adapterView.getContext(), "Selected: " + selectedItem, Toast.LENGTH_LONG).show();
+                languageSelected = selectedItem;
+                Toast.makeText(adapterView.getContext(), "Selected: " + selectedItem, Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
@@ -42,6 +50,46 @@ public class SettingsFragment extends Fragment {
             }
         });
 
+        adapterColour = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, colours);
+        adapterColour.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerColour.setAdapter(adapterColour);
+        spinnerColour.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l){
+                String selectedItem = adapterView.getItemAtPosition(i).toString();
+                colourSelected = selectedItem;
+                Toast.makeText(adapterView.getContext(), "Selected: " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView){
+
+            }
+        });
+        buttonSave.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (colourSelected.equals("Purple")){
+
+
+                } else if (colourSelected.equals("Blue")){
+
+                } else if (colourSelected.equals("Black")){
+
+                } else if (colourSelected.equals("Dark Green")){
+
+                }
+
+                if (languageSelected.equals("English")){
+
+                } else if (languageSelected.equals("Svenska")){
+
+                } else if (languageSelected.equals("Norsk")){
+
+                }
+                Toast.makeText(v.getContext(), "Saved!", Toast.LENGTH_LONG).show();
+            }
+        }));
         return view;
     }
 

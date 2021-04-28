9dd8304c9b64effd6dd6f3ee4b981a60c5305691
kristijan00 <57765162+kristijan00@users.noreply.github.com>
2021-04-28 19:05:07 +0200

    New test class added

---
+++ app/src/test/java/com/mysport/mysport_mobile/forum/CreatePostTest.java
@@ -0,0 +1,10 @@
+package com.mysport.mysport_mobile.forum;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreatePostTest {
+
+    @org.junit.jupiter.api.Test
+    void createPost() {
+    }
+}

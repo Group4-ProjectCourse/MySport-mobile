49f64d94e958ad77f55d560108fb43876088cc84
Miha22 <daniel.neo.eu@icloud.com>
2021-05-17 17:50:28 +0200

    progress with chat

--- app/src/main/java/com/mysport/mysport_mobile/fragments/chat/ChatFragment.java
+++ app/src/main/java/com/mysport/mysport_mobile/fragments/chat/ChatFragment.java
@@ -7,62 +7,66 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.FirebaseDatabase;
 import com.mysport.mysport_mobile.R;
+import com.mysport.mysport_mobile.models.Message;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChatFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class ChatFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FirebaseListAdapter<Message> adapter;
+    private View view;
 
     public ChatFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChatFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ChatFragment newInstance(String param1, String param2) {
         ChatFragment fragment = new ChatFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        this.view = inflater.inflate(R.layout.fragment_chat, container, false);
+        displayAllMessages();
 
         return view;
+    }
+
+    private void displayAllMessages(){
+        ListView messages = findViewById(R.id.chat_messages_list);
+        adapter = new FirebaseListAdapter<Message>(this.getActivity(), Message.class, R.layout.chat_list_item, FirebaseDatabase.getInstance().getReference()) {
+            @Override
+            protected void populateView(View v, Message model, int position) {
+                TextView sender, time, content;
+                sender = findViewById(R.id.chat_item_msg_sender);
+                time = findViewById(R.id.chat_item_msg_time);
+                content = findViewById(R.id.chat_item_msg_content);
+
+                sender.setText(model.getSender());
+                content.setText(model.getContent());
+                time.setText(new SimpleDateFormat());
+            }
+        };
+    }
+
+    public final <T extends View> T findViewById(int id) {
+        return this.view.findViewById(id);
     }
 }
